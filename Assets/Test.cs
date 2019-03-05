8ebbc42561361bb430905d691e808906c2ef4b1c
Rakka Rage <rakkarage@gmail.com>
2019-03-04 20:49:18 -0500

    broke

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -7,4 +7,14 @@ public class Test : ScriptableObject
 	public List<string> Names0;
 	public List<string> Names1;
 	public List<string> Names2;
+	public List<string> Names3;
+	public List<string> Names4;
+	public List<string> Names5;
+	public List<string> Names6;
+	public List<string> Names7;
+	public List<string> Names8;
+	public List<string> Names9;
+	public List<string> Names10;
+	public List<string> Names11;
+	public List<string> Names12;
 }
