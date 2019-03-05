51391b2d0e1b2b337834a595a74991c4af4d2d49
Rakka Rage <rakkarage@gmail.com>
2019-03-04 20:47:13 -0500

    first

---
+++ Assets/Test.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class Test : ScriptableObject
+{
+	public int Id;
+	public List<string> Names0;
+	public List<string> Names1;
+	public List<string> Names2;
+}
